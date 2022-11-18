dea9581f2c22b341ae577bab170e123ecf0a32ae
AliHaidry <alihaidry11@gmail.com>
2022-11-18 15:16:54 -0500

    Code updated with utils,screens folders

---
+++ lib/screens/main_menu_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget{
+  const MainMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text('Main Menu'),
+      ),
+    );
+  }
+}
