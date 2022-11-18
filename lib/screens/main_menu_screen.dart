d3e46d6d241639d9f70674c38f6d86b9aba19033
AliHaidry <alihaidry11@gmail.com>
2022-11-18 17:16:22 -0500

    Code updated with responsive.dart file

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:allsolz_tictactoe_game/widgets/custom_button.dart';
+import 'package:allsolz_tictactoe_game/responsive/responsive.dart';
 
 class MainMenuScreen extends StatelessWidget {
   const MainMenuScreen({Key? key}) : super(key: key);
@@ -7,11 +8,13 @@ class MainMenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        CustomButton(onTap: () {}, text: 'Create Room'),
-        const SizedBox(height: 20),
-        CustomButton(onTap: () {}, text: 'Join Room'),
-      ]),
+      body: Responsive(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          CustomButton(onTap: () {}, text: 'Create Room'),
+          const SizedBox(height: 20),
+          CustomButton(onTap: () {}, text: 'Join Room'),
+        ]),
+      ),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget{
+class CustomButton extends StatelessWidget {
   final VoidCallback onTap;
 
   final String text;
-  const CustomButton({Key? key, required this.onTap, required this.text}) : super(key: key);
+  const CustomButton({Key? key, required this.onTap, required this.text})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return ElevatedButton(
-      onPressed: onTap,
-      child: Text(text, style: const TextStyle(fontSize: 16)),
-      style: ElevatedButton.styleFrom(
+    return Container(
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.pinkAccent,
+            blurRadius: 5,
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        child: Text(text, style: const TextStyle(fontSize: 16)),
+        style: ElevatedButton.styleFrom(
           minimumSize: Size(
             width,
             50,
           ),
         ),
+      ),
     );
   }
-
 }
