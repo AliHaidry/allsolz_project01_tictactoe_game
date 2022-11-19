cd99512ce5b4efe1be6acc1f1ea7d2fe6d1055d2
AliHaidry <alihaidry11@gmail.com>
2022-11-19 15:33:50 -0500

    Code updated.

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:allsolz_tictactoe_game/widgets/custom_text.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create-room';
@@ -12,7 +13,22 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(shadows: [
+                Shadow(
+                  blurRadius: 50,
+                  color: Colors.blue,
+                ),
+              ], text: 'Create Room', fontSize: 70),
+            ]),
+      ),
     );
   }
 }
