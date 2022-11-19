d4cf5835a27849c244ffd12bdee6d623e81ab469
AliHaidry <alihaidry11@gmail.com>
2022-11-19 16:16:15 -0500

    Code updated.

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:allsolz_tictactoe_game/widgets/custom_text.dart';
 import 'package:allsolz_tictactoe_game/widgets/custom_textfield.dart';
 import 'package:allsolz_tictactoe_game/widgets/custom_button.dart';
+import 'package:allsolz_tictactoe_game/responsive/responsive.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create-room';
@@ -25,26 +26,28 @@ class _CreateRoomScreenState extends State<CreateRoomScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(
-          horizontal: 20,
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CustomText(shadows: [
+                  Shadow(
+                    blurRadius: 50,
+                    color: Colors.blue,
+                  ),
+                ], text: 'Create Room', fontSize: 70),
+                SizedBox(height: size.height * 0.08),
+                CustomTextField(
+                    controller: _nameController, hintText: 'Enter your name'),
+                SizedBox(height: size.height * 0.045),
+                CustomButton(onTap: () {}, text: 'Create'),
+              ]),
         ),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const CustomText(shadows: [
-                Shadow(
-                  blurRadius: 50,
-                  color: Colors.blue,
-                ),
-              ], text: 'Create Room', fontSize: 70),
-              SizedBox(height: size.height * 0.08),
-              CustomTextField(
-                  controller: _nameController, hintText: 'Enter your name'),
-              SizedBox(height: size.height * 0.02),
-              CustomButton(onTap: () {}, text: 'Create'),
-            ]),
       ),
     );
   }
--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:allsolz_tictactoe_game/widgets/custom_button.dart';
+import 'package:allsolz_tictactoe_game/widgets/custom_text.dart';
+import 'package:allsolz_tictactoe_game/widgets/custom_textfield.dart';
+import 'package:allsolz_tictactoe_game/responsive/responsive.dart';
+
 
 class JoinRoomScreen extends StatefulWidget {
   static String routeName = '/join-room';
@@ -9,10 +14,47 @@ class JoinRoomScreen extends StatefulWidget {
 }
 
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _gameIdController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _gameIdController.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CustomText(shadows: [
+                  Shadow(
+                    blurRadius: 50,
+                    color: Colors.blue,
+                  ),
+                ], text: 'Join Room', fontSize: 70),
+                SizedBox(height: size.height * 0.08),
+                CustomTextField(
+                    controller: _nameController, hintText: 'Enter your name'),
+                const SizedBox(height: 20),
+                CustomTextField(
+                    controller: _gameIdController, hintText: 'Enter Game ID'),
+                SizedBox(height: size.height * 0.045),
+                CustomButton(onTap: () {}, text: 'Create'),
+              ]),
+        ),
+      ),
     );
   }
 }
