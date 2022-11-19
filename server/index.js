941c5de42a049f6e8363fcf5dbe33d0b413673a5
AliHaidry <alihaidry11@gmail.com>
2022-11-19 17:08:35 -0500

    Code updated.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+// importing modules
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+// client -> middleware -> server
+// middleware
+app.use(express.json());
+
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server Started and Running on Port ${port}`);
+});
+
+
+
