0f88b7451645cdb552e03945620643dba66c0933
AliHaidry <alihaidry11@gmail.com>
2022-11-19 17:54:36 -0500

    Code updated.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // importing modules
 const express = require("express");
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,16 @@ var io = require("socket.io")(server);
 // client -> middleware -> server
 // middleware
 app.use(express.json());
+
+
+const DB = "mongodb+srv://AliHaidry:England1994@cluster0.s3cmb4h.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection is Successful");
+})
+.catch((e) => {
+    console.log(e)
+});
 
 server.listen(port, "0.0.0.0", () => {
     console.log(`Server Started and Running on Port ${port}`);
